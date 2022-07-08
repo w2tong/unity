5f5a2218deddefcf0c51e8d742637a320150a4f1
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-08 15:24:12 -0400

    Add player movement

--- Unit 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
+++ Unit 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float horizontalInput;
+    private float speed = 10.0f;
+    private float xRange = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
     }
 }
