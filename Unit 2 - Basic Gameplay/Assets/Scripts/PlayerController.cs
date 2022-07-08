4808231c859cb2416ffb05463a13a40a7de486c7
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-08 17:06:34 -0400

    Add player veritcal movement and boundaries

--- Unit 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
+++ Unit 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
-    private float speed = 20.0f;
+    private float verticallInput;
+    private float speed = 15.0f;
     private float xRange = 15.0f;
+    private float zBottomRange = -2.5f;
+    private float zTopRange = 15.0f;
 
     [SerializeField] GameObject projectilePrefab;
 
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Prevent player from moving out of bounds
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -29,9 +33,25 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+
+        if (transform.position.z < zBottomRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBottomRange);
+        }
+        if (transform.position.z > zTopRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zTopRange);
+        }
+
+        // Horizontal movement
         horizontalInput = Input.GetAxisRaw("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
 
+        // Vertical movement
+        verticallInput = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.forward * verticallInput * speed * Time.deltaTime);
+
+        // Spawn projectile on space bar
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, transform.rotation);
