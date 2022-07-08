80e7fbc3d787ad654a1469ec7b0d64cbc8db7a97
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-08 15:57:46 -0400

    Create animal prefabs

--- Unit 2 - Basic Gameplay/Assets/Scripts/MoveForward.cs
+++ Unit 2 - Basic Gameplay/Assets/Scripts/MoveForward.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    private float speed = 20.0f;
+    [SerializeField] float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
