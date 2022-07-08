099143f2dad5e9795ccc095370cbd7a030064fd0
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-08 17:58:36 -0400

    Update to spawn animals from left and right

--- Unit 2 - Basic Gameplay/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit 2 - Basic Gameplay/Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,8 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
+    private float leftBound = -35;
+    private float rightBound = 35;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound || transform.position.z < lowerBound)
+        if (transform.position.z > topBound || transform.position.z < lowerBound || transform.position.x < leftBound || transform.position.x > rightBound)
         {
             Destroy(gameObject);
         }
--- Unit 2 - Basic Gameplay/Assets/Scripts/SpawnManager.cs
+++ Unit 2 - Basic Gameplay/Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject[] animalPrefabs;
+    // Top spawn pos
     private float spawnRangeX = 15;
     private float spawnPosZ = 20;
-    private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+
+    // Left and right spawn pos
+    private float spawnRangeZ = 15;
+    private float leftSpawnPosX = -30;
+    private float rightSpawnPosX = 30;
+
+    private float startDelay = 1;
+    private float spawnInterval = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +31,31 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnRandomAnimal()
     {
+        int direction = Random.Range(0, 3);
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        Quaternion rotation = animalPrefabs[animalIndex].transform.rotation;
+
+        switch (direction)
+        {
+            // Top spawn
+            case 0:
+                spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+                rotation = animalPrefabs[animalIndex].transform.rotation;
+                break;
+            // Left spawn
+            case 1:
+                spawnPos = new Vector3(leftSpawnPosX, 0, Random.Range(0, spawnRangeZ));
+                rotation = Quaternion.Euler(0, 90, 0);
+                break;
+            // Right spawn
+            case 2:
+                spawnPos = new Vector3(rightSpawnPosX, 0, Random.Range(0, spawnRangeZ));
+                rotation = Quaternion.Euler(0, -90, 0);
+                break;
+            default:
+                break;
+        }
+        Instantiate(animalPrefabs[animalIndex], spawnPos, rotation);
     }
 }
