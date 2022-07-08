fd7fa6dcce3f1cbb89c98cff3d7f859929a01327
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-08 16:16:57 -0400

    Adjust player movable range

--- Unit 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
+++ Unit 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
     private float speed = 10.0f;
-    private float xRange = 10.0f;
+    private float xRange = 15.0f;
 
     [SerializeField] GameObject projectilePrefab;
 
