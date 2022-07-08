0b2a42ee8da4239e1cf708b13d992204bca598a6
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-08 16:42:27 -0400

    Remove duplicate script

--- Unit 2 - Basic Gameplay/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit 2 - Basic Gameplay/Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestoryOutOfBounds : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
