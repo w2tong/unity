ebc4a7f0ef8fe4209e79383d552f6ee27f1327ef
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-10 00:11:28 -0400

    Add camera rotation and player movement

---
+++ Unit 4 - Gameplay Mechanics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    private float speed = 5;
+    private GameObject focalPoint;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
+    }
+}
