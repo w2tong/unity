cef50c7ac950ead8d411c318b3e4ba070268222f
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-10 01:27:56 -0400

    Spawn enemies and powerups in waves

--- Unit 4 - Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
+++ Unit 4 - Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
@@ -6,17 +6,27 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject enemyPrefab;
     private float spawnRange = 9;
+    private int enemyCount;
+    private int waveNumber = 1;
+
+    [SerializeField] GameObject powerupPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-
-        Instantiate(enemyPrefab, GenerateSpawnPoint(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPoint(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount <= 0)
+        {
+            SpawnEnemyWave(++waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPoint(), powerupPrefab.transform.rotation);
+        }
     }
 
     Vector3 GenerateSpawnPoint()
@@ -24,5 +34,13 @@ public class SpawnManager : MonoBehaviour
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         return new Vector3(spawnPosX, 0, spawnPosZ);
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPoint(), enemyPrefab.transform.rotation);
+        }
     }
 }
