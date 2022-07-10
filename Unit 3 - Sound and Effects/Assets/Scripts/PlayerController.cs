25c07765b4d329d99906f2c8d8726d7d0702abb6
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-09 22:15:03 -0400

    Add movable Player character and spawn obstacles

---
+++ Unit 3 - Sound and Effects/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float jumpForce = 20;
+    private float gravityModifier = 4;
+    private bool isOnGround = true;
+    private float speed = 10;
+    private float xLeftRange = 0;
+    private float xRightRange = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < xLeftRange)
+        {
+            transform.position = new Vector3(xLeftRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRightRange)
+        {
+            transform.position = new Vector3(xRightRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector3.forward * horizontalInput * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
