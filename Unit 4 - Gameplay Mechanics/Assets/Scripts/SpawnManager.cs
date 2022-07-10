1cf39db394630ba32a98a3e6b9911e715b24844f
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-10 00:43:27 -0400

    Spawn enemy at random position

--- Unit 4 - Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
+++ Unit 4 - Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject enemyPrefab;
+    private float spawnRange = 9;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+
+        Instantiate(enemyPrefab, GenerateSpawnPoint(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    Vector3 GenerateSpawnPoint()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        return new Vector3(spawnPosX, 0, spawnPosZ);
     }
 }
