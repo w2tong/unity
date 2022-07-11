79baa21215e7d1b8e24d12ba29c5743d19475422
Wayne Tong <dkotrzarashi@gmail.com>
2022-07-10 20:00:39 -0400

    Add Challenge 2 - Play Fetch

---
+++ Challenge 2 - Play Fetch/Assets/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogSpawnRate = 1.0f;
+    private float dogSpawnTime;
+
+    void Start()
+    {
+        dogSpawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= dogSpawnTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogSpawnTime = Time.time + dogSpawnRate;
+        }
+    }
+}
